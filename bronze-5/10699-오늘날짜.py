9380aa9e32fecfeed5b833cc278ae76580c84052
Leeryeong Song <86554429+leeryeongsong@users.noreply.github.com>
2021-09-03 23:22:05 +0900

    Create 10699-오늘날짜.py

---
+++ bronze-5/10699-오늘날짜.py
@@ -0,0 +1,6 @@
+# https://www.acmicpc.net/problem/10699
+
+import datetime
+print(str(datetime.datetime.now())[:10])
+#print(datetime.datetime.today().strftime("%Y-%m-%d"))
+#print(datetime.datetime.now().strftime("%Y-%m-%d"))
