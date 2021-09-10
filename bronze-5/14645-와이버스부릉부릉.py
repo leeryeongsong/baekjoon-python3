886ab64f443282e5dd89815af0a361e6d747870b
Leeryeong Song <86554429+leeryeongsong@users.noreply.github.com>
2021-09-10 23:13:23 +0900

    Create 14645-와이버스부릉부릉.py

---
+++ bronze-5/14645-와이버스부릉부릉.py
@@ -0,0 +1,10 @@
+# https://www.acmicpc.net/problem/14645
+
+import sys
+
+N, K = map(int, sys.stdin.readline().split())
+
+for i in range(N):
+    A, B = map(int, sys.stdin.readline().split())
+
+print('비와이')
