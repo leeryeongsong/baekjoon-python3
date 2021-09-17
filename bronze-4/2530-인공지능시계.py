41f588dc88526cb3e2392b11bf92b5e6211b6c48
Leeryeong Song <86554429+leeryeongsong@users.noreply.github.com>
2021-09-17 23:24:28 +0900

    Create 2530-인공지능시계.py

---
+++ bronze-4/2530-인공지능시계.py
@@ -0,0 +1,20 @@
+# https://www.acmicpc.net/problem/2530
+"""
+now_hour, now_min, now_sec = map(int, input().split())
+cook_sec = int(input())
+last_sec = (now_sec + cook_sec)%60
+last_min = (now_min + (now_sec + cook_sec)//60)%60
+last_hour = now_hour + (now_min + (now_sec + cook_sec)//60)//60
+if last_hour > 23 :
+    last_hour = last_hour - 24
+
+print(last_hour, last_min, last_sec)
+"""
+
+now_hour, now_min, now_sec = map(int, input().split())
+cook_sec = int(input())
+last_sec = (now_sec + cook_sec)%60
+last_min = (now_min + (now_sec + cook_sec)//60)%60
+last_hour = (now_hour + (now_min + (now_sec + cook_sec)//60)//60)%24
+
+print(last_hour, last_min, last_sec)
