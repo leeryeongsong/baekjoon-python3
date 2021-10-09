e4a7bcef7db2c9b1453fa468bfcc9ad796e845cf
Leeryeong Song <86554429+leeryeongsong@users.noreply.github.com>
2021-10-09 23:59:38 +0900

    Update 5554-심부름가는길.py

--- bronze-5/5554-심부름가는길.py
+++ bronze-5/5554-심부름가는길.py
@@ -2,10 +2,12 @@
 
 sec_sum = 0
 
+# 숫자를 4번 입력받고, 변수 sec_sum에 더하고 저장한다.
 for i in range(4):
     sec_part = int(input())
     sec_sum = sec_sum + sec_part
 
+# 이동 시간 중 분을 min에 저장, 초를 sec에 저장한다.
 min = sec_sum//60
 sec = sec_sum%60
 print(min)
