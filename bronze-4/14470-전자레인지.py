e790806ff4d6950a43b22d3997eadbd8e0286e69
Leeryeong Song <86554429+leeryeongsong@users.noreply.github.com>
2022-01-09 18:50:48 +0900

    Create 14470-전자레인지.py

---
+++ bronze-4/14470-전자레인지.py
@@ -0,0 +1,16 @@
+#https://www.acmicpc.net/problem/14470
+
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+E = int(input())
+time = 0
+
+if A<0:
+    time = -A*C + D
+    A = 0
+
+time = time + (B-A)*E
+
+print(time)
