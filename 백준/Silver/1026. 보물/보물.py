d384075d02cd24d8217eb1bdfb272df9f41a8a04
Leeryeong Song <86554429+leeryeongsong@users.noreply.github.com>
2022-07-27 13:34:40 +0900

    [Silver IV] Title: 보물, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.py
@@ -0,0 +1,10 @@
+sum = 0
+n = int(input())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+a.sort()
+b.sort(reverse=True)
+for i in range(n):
+    sum += a[i] * b[i]
+
+print(sum)
